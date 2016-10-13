cefbe778a9966a6d1bd57a751623a21cfb587c1d
Ruth M. Pardee <ruthmpardee@gmail.com>
2016-10-13 15:49:34 -0700

    Adds solution and new tests for string permutations

--- problems/strings/prompts/string_permutation.js
+++ problems/strings/prompts/string_permutation.js
@@ -10,8 +10,68 @@
   * stringPermutation('what', 'wat') // false
   */
 
-const stringPermutation = function(string1, string2) {
-
+const stringPermutation = (string1, string2) => {
+  // check if the length is the same
+  if (string1.length !== string2.length) {
+    return false
+  } else {
+    // create object with the letters of the first string as keys, and their count as values
+    let s1letters = {};
+    for (let i = 0; i < string1.length; i++) {
+      let s1letter = string1[i];
+      if (!s1letters[s1letter]) {
+        s1letters[s1letter] = 1;
+      } else {
+        s1letters[s1letter] += 1;
+      }
+    }
+    // check to make sure each character in the second string exists in that object
+    for (let i = 0; i < string2.length; i++) {
+      let s1letterCount = s1letters[string2[i]];
+      if (!s1letterCount) {
+        return false;
+      } else {
+        // if it does find the key, decrement it's count in the object
+        s1letters[string2[i]] -= 1;
+        // if after decrementing it's ever negative, it can't be a permutation
+        if (s1letterCount < 0) {
+          return false;
+        }
+      }
+    }
+    // if it gets through the whole thing, return true
+    return true;
+  }
 };
+
+
+console.log("stringPermutation('roar', 'rrrr'): ", stringPermutation('roar', 'rrrr'));
+
+
+const createPermutations = (string) => {
+  let permutations = [];
+  // create subroutine
+  const createPerms = (currentPerm) => {
+    // base case: we've reached the same length as our original string
+    if (currentPerm.length === string.length) {
+      // push into permutations array
+      permutations.push(currentPerm);
+    }
+    // recursive case: not long enough yet
+    else {
+      // loop through the string
+      for (var i = 0; i < string.length; i++) {
+        // add that character to the currentPerm and run through again
+        // currentPerm = currentPerm + string[i];
+        createPerms(currentPerm + string[i]);
+      }
+    }
+  };
+  // call subroutine on string
+  createPerms('');
+  return permutations;
+};
+
+console.log("createPermutations('abc'): ", createPermutations('abc').length);
 
 module.exports = { stringPermutation };
--- problems/strings/tests/string_permutation.test.js
+++ problems/strings/tests/string_permutation.test.js
@@ -11,6 +11,14 @@ describe('Strings', () => {
       assert.equal(solution.stringPermutation('OVO', 'VOO'), true);
 	  });
 
+    it('should identify two strings that are not permutations, but all the letters in the second string are in the first string', () => {
+      assert.equal(solution.stringPermutation('roar', 'rrrr'), false);
+    });
+
+    it('should identify two strings that are not permutations, but all the letters in the first string are in the second string', () => {
+      assert.equal(solution.stringPermutation('rrrr', 'roar'), false);
+    });
+
 	  it('should not identify two strings that are not permutations of each other', () => {
 	    assert.equal(solution.stringPermutation('rawr', 'rawl'), false);
       assert.equal(solution.stringPermutation('wat', 'what'), false);
