ac63eb8bee34cc3c95cb830c84b733a2e8ccd8bd
Ruth M. Pardee <ruthmpardee@gmail.com>
2016-10-20 18:51:17 -0700

    Adds solution for robotPaths and adds robotPath tests to manifest file

--- problems/arrays/prompts/robotPaths.js
+++ problems/arrays/prompts/robotPaths.js
@@ -28,8 +28,110 @@ const makeBoard = n => {
   return board;
 };
 
-const robotPaths = (n, board, i, j) => {
-  // TODO: Implement me!
+
+
+const robotPaths = (n) => {
+  // make counter to increment
+  let counter = 0;
+  let board = makeBoard(n);
+  // make subroutine
+  const countPaths = (i, j) => {
+    // for each iteration, first mark that starting square as visited
+    board.togglePiece(i,j);
+    // base case: I've reached the bottom right corner (5,5)
+    if (i === n - 1 && j === n - 1) {
+      // increment counter
+      counter++;
+      return;
+    //recursive case: Still not at (5,5)
+    } else {
+      // go down (make sure it's not at the bottom, or already visited)
+      if (i <= n - 2 && !board.hasBeenVisited(i + 1, j)) {
+        countPaths(i + 1, j);
+        // once the recursive call is done, untoggle that piece before moving on
+        board.togglePiece(i + 1, j);
+      }
+      // go right (make sure it's not at the right edge, or already visited)
+      if (j <= n - 2 && !board.hasBeenVisited(i, j + 1)) {
+        countPaths(i, j + 1);
+        // once the recursive call is done, untoggle that piece before moving on
+        board.togglePiece(i, j + 1);
+      }
+      // go up (make sure it's not at the top, or already visited)
+      if (i >= 1 && !board.hasBeenVisited(i - 1, j)) {
+        countPaths(i - 1, j);
+        // once the recursive call is done, untoggle that piece before moving on
+        board.togglePiece(i - 1, j);
+      }
+      // go left (make sure it's not at the right edge, or already visited)
+      if (j >= 1 && !board.hasBeenVisited(i, j - 1)) {
+        countPaths(i, j - 1);
+        // once the recursive call is done, untoggle that piece before moving on
+        board.togglePiece(i, j - 1);
+      }
+    }
+  };
+  // call subroutine
+  countPaths(0, 0);
+  return counter;
 }
+
+// LEE'S SOLUTION: (takes longer to run)
+/*
+* func unvisitedNeighbors
+* checks all surrounding choices for unvisited nodes
+**/
+// function unvisitedNeighbors(board, i, j){
+//   return findNeighbors(i, j).filter(function(coordinate){
+//     var row = board[coordinate[0]]
+//     return Boolean(row) && row[coordinate[1]] === false
+//   })
+// }
+
+// function findNeighbors(r, c) {
+//   var above = [r - 1, c],
+//       right = [r, c + 1],
+//       left  = [r, c - 1],
+//       below = [r + 1, c]
+
+//   return [above, below, left, right]
+// }
+
+// function findPaths(n, board, i, j){
+//   board = board || makeBoard(n)
+//   i = i || 0
+//   j = j || 0
+//   // results = results || []
+//   var count = 0
+//   // currentPath = currentPath || []
+//   var neighbors = unvisitedNeighbors(board, i, j)
+
+
+//   //set current board piece to "visited"
+//   board.togglePiece(i, j)
+//   // currentPath.push([i, j])
+
+//   // handle base cases
+//   // if unvisited neighbors exist
+//   if(i === n - 1 && j === n - 1){
+//     // results.push(currentPath.slice(0))
+//     count++
+//   }
+
+//   neighbors.forEach(function(neighbor){
+//     count += findPaths(n, board, neighbor[0], neighbor[1])
+//   })
+
+//   // currentPath.pop()
+//   board.togglePiece(i, j)
+
+//   return count
+// }
+
+
+
+// const robotPaths = (n) => {
+//   return findPaths(n)
+// }
 
 module.exports = { robotPaths };
--- problems/manifest.js
+++ problems/manifest.js
@@ -5,12 +5,13 @@
  */
 
 [
-  // './arrays/tests/flatten.test.js',
   // './arrays/tests/every.test.js',
-  // './arrays/tests/reverse_array_in_place.test.js',
-  // './arrays/tests/some.test.js',
+  // './arrays/tests/flatten.test.js',
   // './arrays/tests/remove_duplicates.test.js',
+  // './arrays/tests/reverse_array_in_place.test.js',
+  './arrays/tests/robotPaths.test.js',
   // './arrays/tests/search_sorted_matrix.test.js',
+  // './arrays/tests/some.test.js',
 // 
   // './async/tests/asyncMap.test.js',
 //
@@ -20,7 +21,7 @@
 //
   // './javascript/tests/arrays.test.js',
   // './javascript/tests/math.test.js',
-  './javascript/tests/hoisting.test.js'
+  // './javascript/tests/hoisting.test.js'
   // './javascript/tests/scoping.test.js',
   // './javascript/tests/async.test.js',
 // 
@@ -43,7 +44,7 @@
   // './strings/tests/isPalindrome.test.js',
   // './strings/tests/longest_palindrome.test.js',
   // './strings/tests/string_compression.test.js',
-  // './strings/tests/string_permutation.test.js',
+  // './strings/tests/string_permutation.test.js'
 // 
   // './trees/tests/basic_tree.test.js',
   // './trees/tests/traverse_tree.test.js',
