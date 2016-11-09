dec735eb5fc8f743ba2de3fd1668c42f6a101c58
Ruth M. Pardee <ruthmpardee@gmail.com>
2016-11-08 16:55:48 -0800

    linkedList solution complete and tests made robust

--- problems/linkedLists/prompts/linkedList.js
+++ problems/linkedLists/prompts/linkedList.js
@@ -3,28 +3,92 @@
 class LinkedList {
 
   constructor() {
-    // TODO: implement constructor
-  }
-
-  contains(val) { // Returns a boolean indicating whether the input value is present
-    // TODO: Implement contains method
-  }
-
-  indexOf(val) { // Returns the numerical order or "index" of the node with value. Returns -1 if not present
-    // TODO: Implement indexOf method
+    this.head = null;
+    this.tail = null;
   }
 
   addToHead(val) { // Adds a node to head
-    // TODO: Implement addToHead method
+    const newNode = {
+      value: val,
+      next: null
+    }
+    if (this.head === null) {
+      // make both the head and tail point to this new node
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      // save the old list
+      const oldList = this.head;
+      // set this old list as the next on the new node we're adding
+      newNode.next = oldList;
+      // set the new head to point to this new node
+      this.head = newNode;
+    }
   }
 
   addToTail(val) { // Adds a node to tail
-    // TODO: Implement addToTail method
+    const newNode = {
+      value: val,
+      next: null
+    };
+    if (this.head === null) {
+      // make both the head and tail point to this new node
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      // set the existing tail to point to this new node
+      this.tail.next = newNode;
+      // reset what the tail points to
+      this.tail = newNode;
+    }
+  }
+ 
+  contains(val) { // Returns a boolean indicating whether the input value is present
+    const findValue = (node) => {
+      // base case 1: found the value
+      if (node.value === val) {
+        return true;
+      // base case 2: no more in the list
+      } else if (node.next === null) {
+        return false;
+      // recursive case
+      } else {
+        return findValue(node.next);
+      }
+    };
+    // invoke the subroutine starting at the head and return the results
+    return findValue(this.head);
   }
 
-  insertAfter(node, val) { 
-    // Given a node and value, it acts similar to addToTail, except it creates a node after the input node
-    // TODO: Implement insertAfter method
+  indexOf(val) { // Returns the numerical order or "index" of the node with value. Returns -1 if not present
+    let counter = 0;
+    // subroutine
+    const findValue = (node) => {
+      // base case 1: we've found the value
+      if (node.value === val) {
+        return counter;
+      // base case 2: no more in the list
+      } else if (node.next === null) {
+        return -1;
+      // recursive case: there is still something next
+      } else {
+        counter++;
+        return findValue(node.next);
+      }
+    };
+    // invoke the subroutine starting at the head and return the results
+    return findValue(this.head);
+  }
+
+  insertAfter(node, val) { // Acts similar to addToTail, except it creates a new node after the input node
+    const restOfList = node.next;
+    // add the rest of the existing list to the new node
+    const newNode = {
+      value: val,
+      next: restOfList
+    };
+    // make the new node what's next with the node passed in
+    node.next = newNode;
   }
 }
 
--- problems/linkedLists/tests/linkedList.test.js
+++ problems/linkedLists/tests/linkedList.test.js
@@ -2,50 +2,144 @@
 
 const LinkedList = require('../prompts/linkedList.js').LinkedList,
       assert = require('chai').assert;
+let myList;
 
 describe('LinkedLists', () => {
   describe('LinkedList', () => {
-    it('should add items to head and tail', () => {
-      const myList = new LinkedList();
 
-      assert.equal(myList.head, null, 'Empty initial list');
-      myList.addToTail('is how ');
-      myList.addToTail('we do it');
-      assert.equal(myList.tail.value, 'we do it', 'Tail insertion not working');
-      assert.equal(myList.head.value, 'is how ', 'Tail insertion not working');
-      myList.addToHead('This ');
-      assert.equal(myList.head.value, 'This ', 'Head insertion not working');
-      assert.equal(myList.tail.value, 'we do it', 'Tail insertion not working');
-
-      const montellJordan = 'This is how we do it';
-      let output = '';
-
-      for(let i = myList.head; i; i = i.next){
-        output += i.value;
-      }
-      assert.equal(output, montellJordan, 'Head and Tail insertion methods not working');
+    beforeEach(() => {
+      myList = new LinkedList();
     });
 
-    it('should insert items after the passed in node', () => {
-      const myList = new LinkedList();
-      let fourthNode = null;
-      for (let i = 0; i < 10; i++) {
-        myList.addToTail(i);
-        if (i === 3) {
-          fourthNode = myList.tail;
+    describe('constructor', () => {
+      it('should create a new list with a null head and tail', () => {
+        // const myList = new LinkedList();
+        assert.equal(myList.head, null, 'Head not null with new list creation');
+        assert.equal(myList.tail, null, 'Tail not null with new list creation');
+      });
+    });
+
+    describe('addToHead', () => {
+      it('should add items to head if list is empty', () => {
+        // const myList = new LinkedList();
+        myList.addToHead('is my jam');
+        assert.equal(myList.head.value, 'is my jam', 'Head insertion not working for empty list');
+      });
+
+      it('should update tail when adding items to head if list is empty', () => {
+        // const myList = new LinkedList();
+        myList.addToHead('is my jam');
+        assert.equal(myList.tail.value, 'is my jam', 'Head insertion not updating tail to same value when first item');
+        assert.equal(myList.tail, myList.head, 'Head insertion not updating tail to point to same node when first item');
+      });
+
+      it('should add items to head if list is not empty', () => {
+        // const myList = new LinkedList();
+        myList.addToHead('is my jam');
+        myList.addToHead('This ');
+        assert.equal(myList.head.value, 'This ', 'Head insertion not working for existing list');
+      });
+
+      it('should not update tail when adding items to head if list is not empty', () => {
+        // const myList = new LinkedList();
+        myList.addToHead('is my jam');
+        myList.addToHead('This ');
+        assert.equal(myList.tail.value, 'is my jam', 'Head insertion not preserving existing tail when list is not empty');
+        assert.notEqual(myList.tail, myList.head, 'Head insertion not preserving existing tail when list is not empty');
+      });
+    });
+
+    describe('addToTail', () => {
+      it('should add items to tail if list is empty', () => {
+        // const myList = new LinkedList();
+        myList.addToTail('This ');
+        assert.equal(myList.tail.value, 'This ', 'Tail insertion not working for empty list');
+      });
+
+      it('should update head when adding items to tail if list is empty', () => {
+        // const myList = new LinkedList();
+        myList.addToTail('This ');
+        assert.equal(myList.head.value, 'This ', 'Tail insertion not updating head to same value when first item');
+        assert.equal(myList.head, myList.tail, 'Tail insertion not updating head to point to same node when first item');
+      });
+
+      it('should add items to tail if list is not empty', () => {
+        // const myList = new LinkedList();
+        myList.addToTail('This ');
+        myList.addToTail('is my jam');
+        assert.equal(myList.tail.value, 'is my jam', 'Tail insertion not working for existing list');
+      });
+
+      it('should not update head when adding items to tail if list is not empty', () => {
+        // const myList = new LinkedList();
+        myList.addToTail('This ');
+        myList.addToTail('is my jam');
+        assert.equal(myList.head.value, 'This ', 'Tail insertion not preserving existing head when list is not empty');
+        assert.notEqual(myList.head, myList.tail, 'Tail insertion not preserving existing head when list is not empty');
+      });
+    });
+
+    describe('contains', () => {
+      it('should return true if the value exists anywhere in the list', () => {
+        // const myList = new LinkedList();
+        myList.addToTail('is how ');
+        myList.addToTail('we do it');
+        myList.addToHead('This ');
+        assert.isTrue(myList.contains('This '), 'Contains not finding item in head of list');
+        assert.isTrue(myList.contains('is how '), 'Contains not finding item in body of list');
+        assert.isTrue(myList.contains('we do it'), 'Contains not finding item in tail of list');
+      });
+
+      it('should return false if the value does not exist anywhere in the list', () => {
+        // const myList = new LinkedList();
+        myList.addToTail('is how ');
+        myList.addToTail('we do it');
+        myList.addToHead('This ');
+        assert.isNotTrue(myList.contains('montellJordan'), 'Contains not working for items not in the list');
+      });
+    });
+    
+    describe('indexOf', () => {
+      it('should return the index if the value exists anywhere in the list', () => {
+        // const myList = new LinkedList();
+        myList.addToTail('is how ');
+        myList.addToTail('we do it');
+        myList.addToHead('This ');
+        assert.equal(myList.indexOf('This '), 0, 'indexOf not finding item in head of list');
+        assert.equal(myList.indexOf('is how '), 1, 'indexOf not finding item in body of list');
+        assert.equal(myList.indexOf('we do it'), 2, 'indexOf not finding item in tail of list');
+      });
+
+      it('should return -1 if the value does not exist anywhere in the list', () => {
+        // const myList = new LinkedList();
+        myList.addToTail('is how ');
+        myList.addToTail('we do it');
+        myList.addToHead('This ');
+        assert.equal(myList.indexOf('montellJordan'), -1, 'indexOf not working for items not in the list');
+      });
+    });
+
+    describe('insertAfter', () => {
+      it('should insert items after the passed in node', () => {
+        // const myList = new LinkedList();
+        let fourthNode = null;
+        for (let i = 0; i < 10; i++) {
+          myList.addToTail(i);
+          if (i === 3) {
+            fourthNode = myList.tail;
+          }
         }
-      }
-      for (let i = 0; i < 10; i++) {
-        myList.addToTail(i);
-      }
-      myList.insertAfter(fourthNode, 3.14159);
-      assert.isTrue(myList.contains(3.14159));
-      const soln = [0,1,2,3,3.14159,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9];
-      let current = myList.head;
-      for (let i = 0; i < soln.length; i++) {
-        assert.equal(current.value, soln[i], 'Should only insert once');
-        current = current.next;
-      }
+        for (let i = 0; i < 10; i++) {
+          myList.addToTail(i);
+        }
+        myList.insertAfter(fourthNode, 3.14159);
+        const soln = [0,1,2,3,3.14159,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9];
+        let current = myList.head;
+        for (let i = 0; i < soln.length; i++) {
+          assert.equal(current.value, soln[i], 'Should only insert once');
+          current = current.next;
+        }
+      });
     });
   });
 });
