197cece70d4d881e218b5d84fd6ead016f62ab2a
Ruth M. Pardee <ruthmpardee@gmail.com>
2016-11-29 18:39:24 -0800

    Simplified version combining mine and Richard's.

--- problems/math/prompts/max_product.js
+++ problems/math/prompts/max_product.js
@@ -20,85 +20,41 @@
   * Because the largest product can be obtained by 3, 3, 2.
   */  
 
-const allOptions = int => {
+const maxProduct = int => {
   switch (int) {
-    // To create fewer cases for maxProduct, the default code includes nothing with 1's
-    // therefore I must deal with these edge cases first and hardcode them
-    case 1:
-      return [[1]];
-    case 2:
-      return [[1,1]];
-    case 3:
-      return [[2,1]];
-    default:
-      const permutations = [];
-      // To create fewer cases for maxProduct, the lowest number included
-      // in the permutations should grow as the magnitude of the int grows
-      // The default is 2 for any single or double digit number
-      let startAt = 2;
-      // For any number with 3 or more digits, we should start at however many digits it has
-      const places = int.toString().length;
-      if (places > 2) {
-        startAt = places;
-      }
-      // We can always stop at just above it's square root
-      let stopAt = Math.ceil(Math.sqrt(int));
+  // To create fewer cases for maxProduct, the default code includes nothing with 1's
+  // therefore I must deal with these edge cases first and hardcode them
+  case 1:
+    return 1;
+  case 2:
+    return 1;
+  case 3:
+    return 2;
+  default:
 
-      (function makePerms(nums = [], sum = 0){
-        // const sum = nums.reduce((currentSum, number) => currentSum + number, 0);
-        if (sum === int) {
-          permutations.push(nums);
-        } else if (sum < int) {
-          for (let i = startAt; i <= stopAt; i++) {
-            makePerms(nums.concat(i), sum + i);
-          }
+    let largestProduct = 0;
+
+    // To create fewer cases, we don't need to check 1's
+    let startAt = 2;
+    // We can always stop at just above it's square root
+    let stopAt = Math.ceil(Math.sqrt(int));
+
+    (function findLargest(product = 1, sum = 0){
+      // if we've reached our integer and beaten our previous largest
+      if (sum === int && product > largestProduct) {
+        largestProduct = product;
+      // if we still haven't reached our integer
+      } else if (sum < int) {
+        // loop through all integers from start -> stop
+        for (let number = startAt; number <= stopAt; number++) {
+          // multiply each one to make a new product, and add each one for a new sum
+          findLargest(product * number, sum + number);
         }
-      })();
+      }
+    })();
 
-      return permutations;
+    return largestProduct;
   }
 };
 
-const maxProduct = int => {
-  let largestProduct = {
-    product: null,
-    numbers: null
-  };
-  const options = allOptions(int);
-  options.forEach(option => {
-    const product = option.reduce((currProduct, number) => currProduct * number, 1);
-    if (product > largestProduct.product) {
-      largestProduct.product = product;
-      largestProduct.numbers = option;
-    }
-  });
-  console.log(`largestProduct for ${int}: `, largestProduct);
-  return largestProduct;
-};
-
-/*
-Rich's solution:
-const maxProduct = int => {
-
-  let largest = 0;
-  const stopAt = Math.ceil(Math.sqrt(int));
-
-  (function findProduct(product = 1, sum = 0){
-    if (sum === int && product > largest){
-      largest = product;
-    // if we haven't reached our integer yet
-    } else if (sum < int) {
-      // loop from 2 to the square root of the integer
-      for (let number = 2; number <= stopAt; number++) {
-        // if the sum is at least greater than the number we're checking
-          // recursively call, incrementing the product and the sum by this number
-          findProduct(product * number, sum + number);
-      }
-    }
-  })();
-
-  return largest;
-};
-*/
-
-module.exports = { allOptions, maxProduct };
+module.exports = { maxProduct };
--- problems/math/tests/max_product.test.js
+++ problems/math/tests/max_product.test.js
@@ -4,47 +4,24 @@ const solution = require('../prompts/max_product.js'),
       assert = require ('chai').assert;
 
 describe('Math', () => {
-  describe('allOptions', () => {
-
-    it('should return an array of arrays of numbers', () => {
-      const three = solution.allOptions(3);
-      assert.isArray(three, "fn should return an array");
-      three.forEach(arrayOfNums => {
-        assert.isArray(arrayOfNums, "the array returned should be composed of other arrays");
-        arrayOfNums.forEach(number => {
-          assert.isNumber(number, "the inner arrays should contain only numbers");
-        })
-      });
-    });
-
-    it('should find all permutations of single digit numbers', () => {
-      assert.deepEqual(solution.allOptions(4), [[2,2]], 'returns array of all possible breakdowns for 4');
-    });
-
-    it('should work for edge cases and find all permutations of small single digit numbers', () => {
-      assert.deepEqual(solution.allOptions(1), [[1]], 'returns array of array of 1 for 1');
-      assert.deepEqual(solution.allOptions(2), [[1,1]], 'returns array of array of [1,1] for 2');
-      assert.deepEqual(solution.allOptions(3), [[2,1]], 'returns array of arrays of all possible breakdowns for 3');
-    });
-  });
 
   describe('maxProduct', () => {
 
     it('should find the largest product of single digit numbers', () => {
-      assert.strictEqual(solution.maxProduct(6).product, 9);
-      assert.strictEqual(solution.maxProduct(8).product, 18);
-      assert.strictEqual(solution.maxProduct(9).product, 27);
+      assert.strictEqual(solution.maxProduct(6), 9);
+      assert.strictEqual(solution.maxProduct(8), 18);
+      assert.strictEqual(solution.maxProduct(9), 27);
     });
 
     it('should work for edge cases and find the largest product of small single digit numbers', () => {
-      assert.strictEqual(solution.maxProduct(1).product, 1, 'returns 1 as the maxProduct fo.productr 1');
-      assert.strictEqual(solution.maxProduct(2).product, 1, 'returns 1 as the maxProduct fo.productr 2');
-      assert.strictEqual(solution.maxProduct(3).product, 2, 'returns 2 as the maxProduct fo.productr 3');
+      assert.strictEqual(solution.maxProduct(1), 1, 'returns 1 as the maxProduct for 1');
+      assert.strictEqual(solution.maxProduct(2), 1, 'returns 1 as the maxProduct for 2');
+      assert.strictEqual(solution.maxProduct(3), 2, 'returns 2 as the maxProduct for 3');
     });
 
     it('should find the largest product of multiple digit numbers', () => {
-      assert.strictEqual(solution.maxProduct(20).product, 1458);
-      // assert.strictEqual(solution.maxProduct(100).product, 7412080755407364);
+      assert.strictEqual(solution.maxProduct(20), 1458);
+      // assert.strictEqual(solution.maxProduct(100), 7412080755407364);
     });
   });
 });
